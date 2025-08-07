15c4de9d6ba01d731ccf3e0bce873d4b5d7d854c
Steve Gehrman <sgehrman@gmail.com>
2025-08-06 22:25:32 -0700

    bundling llama_cpp

--- src/update_llama.sh
+++ src/update_llama.sh
@@ -17,6 +17,10 @@ rm -rf tests
 rm -rf .github
 rm -rf .git
 rm -rf vendor
+rm -rf prompts
+rm -rf tools
+rm -rf scripts
+rm -rf gguf-py
 
 rm .gitignore
 rm .clang-format
@@ -26,4 +30,20 @@ rm CODEOWNERS
 rm LICENSE
 rm README.md
 rm SECURITY.md
- 
+rm .dockerignore
+rm .gitmodules
+rm .pre-commit-config.yaml
+rm .flake8
+rm CONTRIBUTING.md
+rm AUTHORS
+rm .ecrc
+rm convert_hf_to_gguf.py
+rm convert_hf_to_gguf_update.py
+rm convert_llama_ggml_to_gguf.py
+rm convert_lora_to_gguf.py
+rm flake.lock
+rm flake.nix
+rm poetry.lock
+rm pyproject.toml
+rm pyrightconfig.json
+rm mypy.ini
