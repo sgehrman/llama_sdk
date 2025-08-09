bdbb33755f352929459e22fdee757e1edcfa7fb6
Steve Gehrman <sgehrman@gmail.com>
2025-08-08 20:54:54 -0700

    revert

--- src/llama_cpp/ggml/src/ggml.c
+++ src/llama_cpp/ggml/src/ggml.c
@@ -474,16 +474,10 @@ bool ggml_guid_matches(ggml_guid_t guid_a, ggml_guid_t guid_b) {
 }
 
 const char * ggml_version(void) {
-#ifndef GGML_VERSION
-#define GGML_VERSION "dev"
-#endif
     return GGML_VERSION;
 }
 
 const char * ggml_commit(void) {
-#ifndef GGML_COMMIT
-#define GGML_COMMIT "unknown"
-#endif
     return GGML_COMMIT;
 }
 
